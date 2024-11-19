b5f28529fc036240dd0164556eea61a16c6e58ca
Celian Raimbault <celian.raimbault@datadoghq.com>
2024-11-19 17:54:27 +0100

    [logging] logging module test

---
+++ src/deva/utils/logging.py
@@ -0,0 +1,22 @@
+def error(message: str):
+    """Log error message."""
+
+    print(f"Error: {message}")
+
+
+def warning(message: str):
+    """Log error message."""
+
+    print(f"Warning: {message}")
+
+
+def note(message: str):
+    """Log note message."""
+
+    print(f"Note: {message}")
+
+
+def info(message: str):
+    """Log info message."""
+
+    print(f"Note: {message}")
