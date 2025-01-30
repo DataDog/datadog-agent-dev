9711e279d2c74ff301b24d2dc51822c9bc741f94
Ofek Lev <ofekmeister@gmail.com>
2025-01-29 21:39:36 -0500

    Add GitHub action for installation

---
+++ README.md
@@ -0,0 +1,42 @@
+# Install deva Action
+
+-----
+
+This is an action to install deva in your GitHub Actions workflow.
+
+## Usage
+
+You must [use](https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#jobsjob_idstepsuses) this action in one of your [jobs](https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#jobs)' [steps](https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#jobsjob_idsteps):
+
+```yaml
+- name: Install deva
+  uses: DataDog/datadog-agent-dev@install
+```
+
+For strict security guarantees, it's best practice to [pin](https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#example-using-versioned-actions) the action to a specific commit (of the [`install` branch](https://github.com/DataDog/datadog-agent-dev/tree/install)) like so:
+
+```yaml
+- name: Install deva
+  uses: DataDog/datadog-agent-dev@install
+```
+
+## Options
+
+Name | Default | Description
+--- | --- | ---
+`version` | `latest` | The version of deva to install (e.g. `0.4.2`).
+
+## External consumers
+
+It's possible to use the [install script](https://github.com/DataDog/datadog-agent-dev/blob/install/main.sh) outside of GitHub Actions assuming you set up your environment as follows:
+
+- Set every [option](#options) to an environment variable prefixed by `DEVA_INSTALL_`, uppercased and with hyphens replaced by underscores.
+- Set the `DEVA_INSTALL_PATH` environment variable to the directory where you want to install deva.
+- Set the `DEVA_INSTALL_PLATFORM` environment variable to the current platform using one of the following values:
+    - `linux`
+    - `windows`
+    - `macos`
+- Set the `DEVA_INSTALL_ARCH` environment variable to the current architecture using one of the following values:
+    - `X64`
+    - `ARM64`
+- Install [pipx](https://github.com/pypa/pipx) as a fallback installation method for when there is no [standalone binary](https://deva.pypa.io/latest/install/#standalone-binaries) available. In this case, nothing will be written to `GITHUB_PATH`.
---
+++ main.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+PURPLE="\033[1;35m"
+RESET="\033[0m"
+TARGET_PLATFORM=${DEVA_INSTALL_PLATFORM:-${RUNNER_OS}}
+TARGET_PLATFORM=${TARGET_PLATFORM,,}
+TARGET_ARCH=${DEVA_INSTALL_ARCH:-${RUNNER_ARCH}}
+TARGET_ARCH=${TARGET_ARCH,,}
+
+if [[ "${TARGET_PLATFORM}" == "windows" ]]; then
+  SEP="\\"
+else
+  SEP="/"
+fi
+INSTALL_PATH="${DEVA_INSTALL_PATH:-${RUNNER_TOOL_CACHE}${SEP}.deva}"
+
+install_deva() {
+  mkdir -p "${INSTALL_PATH}"
+  archive="${INSTALL_PATH}${SEP}$1"
+
+  echo -e "${PURPLE}Downloading deva ${DEVA_INSTALL_VERSION}${RESET}\n"
+  if [[ "${DEVA_INSTALL_VERSION}" == "latest" ]]; then
+    curl -sSLo "${archive}" "https://github.com/DataDog/datadog-agent-dev/releases/latest/download/$1"
+  else
+    curl -sSLo "${archive}" "https://github.com/DataDog/datadog-agent-dev/releases/download/deva-v${DEVA_INSTALL_VERSION}/$1"
+  fi
+
+  if [[ "${archive}" =~ \.zip$ ]]; then
+    if [[ "${TARGET_PLATFORM}" == "windows" ]]; then
+      7z -bso0 -bsp0 x "${archive}" -o"${INSTALL_PATH}"
+    else
+      unzip "${archive}" -d "${INSTALL_PATH}"
+    fi
+  else
+    tar -xzf "${archive}" -C "${INSTALL_PATH}"
+  fi
+  rm "${archive}"
+
+  echo -e "${PURPLE}Installing deva ${DEVA_INSTALL_VERSION}${RESET}"
+  "${INSTALL_PATH}${SEP}deva" --version
+  "${INSTALL_PATH}${SEP}deva" self cache dist --remove
+
+  if [[ "${GITHUB_ACTIONS:-}" == "true" ]]; then
+    echo "${INSTALL_PATH}" >> "${GITHUB_PATH}"
+  fi
+}
+
+fallback_install_deva() {
+  echo -e "${PURPLE}Installing deva ${DEVA_INSTALL_VERSION}${RESET}"
+  if [[ "${DEVA_INSTALL_VERSION}" == "latest" ]]; then
+    pipx install --pip-args=--upgrade deva
+  else
+    pipx install "deva==${DEVA_INSTALL_VERSION}"
+  fi
+
+  deva --version
+}
+
+if [[ "${TARGET_PLATFORM}" == "linux" ]]; then
+  if [[ "${TARGET_ARCH}" == "x64" ]]; then
+    install_deva "deva-x86_64-unknown-linux-gnu.tar.gz"
+  elif [[ "${TARGET_ARCH}" == "ARM64" ]]; then
+    install_deva "deva-aarch64-unknown-linux-gnu.tar.gz"
+  else
+    fallback_install_deva
+  fi
+elif [[ "${TARGET_PLATFORM}" == "windows" ]]; then
+  if [[ "${TARGET_ARCH}" == "x64" ]]; then
+    install_deva "deva-x86_64-pc-windows-msvc.zip"
+  else
+    fallback_install_deva
+  fi
+elif [[ "${TARGET_PLATFORM}" == "macos" ]]; then
+  if [[ "${TARGET_ARCH}" == "ARM64" ]]; then
+    install_deva "deva-aarch64-apple-darwin.tar.gz"
+  elif [[ "${TARGET_ARCH}" == "x64" ]]; then
+    install_deva "deva-x86_64-apple-darwin.tar.gz"
+  else
+    fallback_install_deva
+  fi
+else
+  fallback_install_deva
+fi
