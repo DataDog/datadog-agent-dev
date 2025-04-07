354a8fb4c4ad55e088d2343b511ded22852d52cc
Kevin Fairise <kevin.fairise@datadoghq.com>
2025-04-07 11:01:58 +0200

    Apply suggestions

--- src/dda/env/dev/types/linux_container.py
+++ src/dda/env/dev/types/linux_container.py
@@ -4,7 +4,7 @@
 from __future__ import annotations
 
 from functools import cached_property
-from typing import TYPE_CHECKING, Annotated, Any, Literal, NoReturn
+from typing import TYPE_CHECKING, Annotated, Any, Literal, NoReturn, Optional
 
 import msgspec
 
@@ -50,13 +50,14 @@ class LinuxContainerConfig(DeveloperEnvironmentConfig):
         ),
     ] = "zsh"
     arch: Annotated[
-        Literal["amd64", "arm64"],
+        Optional[str],
         msgspec.Meta(
             extra={
                 "help": "The architecture to use e.g. `amd64` or `arm64`",
             }
         ),
-    ] = "arm64"
+    ] = None
+
 
 class LinuxContainer(DeveloperEnvironmentInterface[LinuxContainerConfig]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
@@ -88,7 +89,10 @@ class LinuxContainer(DeveloperEnvironmentInterface[LinuxContainerConfig]):
             from dda.utils.retry import wait_for
 
             if not self.config.no_pull:
-                self.docker.wait(["pull", self.config.image, "--platform", f"linux/{self.config.arch}"], message=f"Pulling image: {self.config.image}")
+                pull_command = ["pull", self.config.image]
+                if self.config.arch is not None:
+                    pull_command.extend(["--platform", f"linux/{self.config.arch}"])
+                self.docker.wait(pull_command, message=f"Pulling image: {self.config.image}")
 
             self.shared_dir.ensure_dir()
             command = [
@@ -96,8 +100,6 @@ class LinuxContainer(DeveloperEnvironmentInterface[LinuxContainerConfig]):
                 "--pull",
                 "never",
                 "-d",
-                "--platform",
-                f"linux/{self.config.arch}",
                 "--name",
                 self.container_name,
                 "-p",
@@ -107,6 +109,9 @@ class LinuxContainer(DeveloperEnvironmentInterface[LinuxContainerConfig]):
                 "-e",
                 AppEnvVars.TELEMETRY_API_KEY,
             ]
+            if self.config.arch is not None:
+                command.extend(["--platform", f"linux/{self.config.arch}"])
+
             for shared_shell_file in self.shell.collect_shared_files():
                 unix_path = shared_shell_file.relative_to(self.global_shared_dir).as_posix()
                 command.extend(("-v", f"{shared_shell_file}:{self.home_dir}/.shared/{unix_path}"))
