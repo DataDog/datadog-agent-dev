5bfc60b319280e302e40798a87b089d73ad090eb
Pierre-Louis Veyrenc <pierrelouis.veyrenc@datadoghq.com>
2025-06-19 17:19:21 +0200

    feat(fs): Create `change_workdir` context manager

--- src/dda/utils/fs.py
+++ src/dda/utils/fs.py
@@ -137,3 +137,27 @@ def temp_directory() -> Generator[Path, None, None]:
 
     with TemporaryDirectory() as d:
         yield Path(d).resolve()
+
+
+@contextmanager
+def change_workdir(new_workdir: Path) -> Generator[None, None, None]:
+    """
+    A context manager that temporarily changes the current working directory to the specified path.
+    Example:
+
+    ```python
+    with change_workdir("foo"):
+        ...
+    ```
+
+    Yields:
+        None
+    """
+    new_workdir = Path(new_workdir).expand()
+    old_workdir = Path.cwd().expand()
+    os.chdir(new_workdir)
+
+    try:
+        yield
+    finally:
+        os.chdir(old_workdir)
