3aed29d5ace89bc4bea3ae3ce9379dccd1ebf48f
Pierre-Louis Veyrenc <pierrelouis.veyrenc@datadoghq.com>
2025-10-07 15:05:13 +0200

    refactor(tests): Pre-create test file structures instead of creating on run
    This allows us to make clearer the example file structures we are testing against, and also avoids the need for file creation fixtures.

--- tests/cli/info/owners/test_code.py
+++ tests/cli/info/owners/test_code.py
@@ -10,92 +10,51 @@ import pytest
 from dda.utils.fs import Path
 
 if TYPE_CHECKING:
-    from collections.abc import Callable, Iterable
     from types import ModuleType
 
     from tests.conftest import CliRunner
 
+TESTCASE_RESULTS = [
+    {
+        "testfile.txt": ["@owner1"],
+    },
+    {
+        "testfile.txt": ["@owner1", "@owner2"],
+    },
+    {
+        "testfile.txt": ["@owner2"],
+        "otherfile.txt": ["@owner1"],
+    },
+    {
+        "subdir1/": ["@owner1"],
+        "subdir1/testfile1.txt": ["@owner1"],
+        "subdir2/": ["@owner2"],
+        "subdir2/testfile2.txt": ["@owner3"],
+    },
+    {
+        "test.txt": ["@DataDog/team-everything"],
+        "README.md": [
+            "@DataDog/team-devops",
+            "@DataDog/team-doc",
+        ],
+        ".gitlab/": ["@DataDog/team-devops"],
+        ".gitlab/security.yml": ["@DataDog/team-security"],
+        ".gitlab/ci.yml": ["@DataDog/team-devops"],
+    },
+]
+
 
 @pytest.mark.parametrize(
-    ("ownership_data", "expected_result"),
-    [
-        # Test case 1: Single owner for a single file
-        (
-            {
-                "testfile.txt": ["@owner1"],
-            },
-            None,  # expected_result will default to ownership_data
-        ),
-        # Test case 2: Multiple owners for a single file
-        (
-            {
-                "testfile.txt": ["@owner1", "@owner2"],
-            },
-            None,
-        ),
-        # Test case 3: Wildcard ownership
-        (
-            {
-                "*.txt": ["@owner1"],
-                "testfile.txt": ["@owner2"],
-            },
-            {
-                "testfile.txt": ["@owner2"],
-                "otherfile.txt": ["@owner1"],
-            },
-        ),
-        # Test case 4: Directory ownership
-        (
-            {
-                "subdir1": ["@owner1"],
-                "subdir2": ["@owner2"],
-                "subdir2/testfile2.txt": ["@owner3"],
-            },
-            {
-                "subdir1/": ["@owner1"],
-                "subdir1/testfile1.txt": ["@owner1"],
-                "subdir2/": ["@owner2"],
-                "subdir2/testfile2.txt": ["@owner3"],
-            },
-        ),
-        # Test case 5: Complicated situation with multiple patterns
-        (
-            {
-                "*": ["@DataDog/team-everything"],
-                "*.md": ["@DataDog/team-devops", "@DataDog/team-doc"],
-                ".gitlab": ["@DataDog/team-devops"],
-                ".gitlab/security.yml": ["@DataDog/team-security"],
-            },
-            {
-                "test.txt": ["@DataDog/team-everything"],
-                "README.md": [
-                    "@DataDog/team-devops",
-                    "@DataDog/team-doc",
-                ],
-                ".gitlab/": ["@DataDog/team-devops"],
-                ".gitlab/security.yml": ["@DataDog/team-security"],
-                ".gitlab/ci.yml": ["@DataDog/team-devops"],
-            },
-        ),
-    ],
+    ("case_number", "expected_result"),
+    enumerate(TESTCASE_RESULTS),
 )
 def test_ownership_parsing(  # type: ignore[no-untyped-def]
     dda: CliRunner,
-    temp_dir: Path,
-    create_codeowners_file: Callable[[dict[str, list[str]], Path], None],
-    create_temp_items: Callable[[Iterable[str], Path], None],
-    default_codeowners_location: Path,
-    ownership_data: dict[str, list[str]],
-    expected_result: dict[str, list[str]] | None,
+    case_number: int,
+    expected_result: dict[str, list[str]],
     helpers: ModuleType,
 ) -> None:
-    if expected_result is None:
-        expected_result = ownership_data
-    files = expected_result.keys()
-    create_codeowners_file(ownership_data, temp_dir / default_codeowners_location)
-    create_temp_items(files, temp_dir)
-
-    with temp_dir.as_cwd():
+    with (Path(__file__).parent / "fixtures" / f"test{case_number}").as_cwd():
         result = dda(
             "info",
             "owners",
@@ -117,94 +76,43 @@ def test_ownership_parsing(  # type: ignore[no-untyped-def]
 
 def test_ambiguous_directory_paths(
     dda: CliRunner,
-    temp_dir: Path,
-    create_codeowners_file: Callable[[dict[str, list[str]], Path], None],
-    create_temp_items: Callable[[Iterable[str], Path], None],
-    default_codeowners_location: Path,
     helpers: ModuleType,
 ) -> None:
-    ownership_data = {
+    paths_to_test = [
+        "dir_with_slash",
+        "dir_without_slash",
+        "dir_with_slash/",
+        "dir_without_slash/",
+        "dir_with_slash/testfile.txt",
+        "dir_without_slash/testfile.txt",
+        "dir_with_slash/subdir_with_slash/",
+        "dir_with_slash/subdir_without_slash/",
+        "dir_with_slash/subdir_with_slash",
+        "dir_with_slash/subdir_without_slash",
+        "dir_with_slash/subdir_with_slash/testfile.txt",
+        "dir_with_slash/subdir_without_slash/testfile.txt",
+    ]
+
+    # Note that the paths referring to directories have a trailing slash
+    # And that this does not depend on whether they were _passed_ with a trailing slash or not.
+    expected_result = {
         "dir_with_slash/": ["@owner1"],
+        "dir_without_slash/": ["@owner3"],
         "dir_with_slash/testfile.txt": ["@owner2"],
-        "dir_without_slash": ["@owner3"],
         "dir_without_slash/testfile.txt": ["@owner4"],
         "dir_with_slash/subdir_with_slash/": ["@owner5"],
         "dir_with_slash/subdir_with_slash/testfile.txt": ["@owner6"],
-        "dir_with_slash/subdir_without_slash": ["@owner7"],
+        "dir_with_slash/subdir_without_slash/": ["@owner7"],
         "dir_with_slash/subdir_without_slash/testfile.txt": ["@owner8"],
     }
-    create_codeowners_file(ownership_data, temp_dir / default_codeowners_location)
-    create_temp_items(
-        (
-            "dir_with_slash/testfile.txt",
-            "dir_without_slash/testfile.txt",
-            "dir_with_slash/subdir_with_slash/testfile.txt",
-            "dir_with_slash/subdir_without_slash/testfile.txt",
-        ),
-        temp_dir,
-    )
-    with temp_dir.as_cwd():
+
+    with (Path(__file__).parent / "fixtures" / "test_ambiguous_dirs").as_cwd():
         result = dda(
             "info",
             "owners",
             "code",
             "--json",
-            "dir_with_slash",
-            "dir_without_slash",
-            "dir_with_slash/",
-            "dir_without_slash/",
-            "dir_with_slash/testfile.txt",
-            "dir_without_slash/testfile.txt",
-            "dir_with_slash/subdir_with_slash/",
-            "dir_with_slash/subdir_without_slash/",
-            "dir_with_slash/subdir_with_slash",
-            "dir_with_slash/subdir_without_slash",
-            "dir_with_slash/subdir_with_slash/testfile.txt",
-            "dir_with_slash/subdir_without_slash/testfile.txt",
-        )
-    result.check(
-        exit_code=0,
-        stdout_json={
-            "dir_with_slash/": ["@owner1"],
-            "dir_without_slash/": ["@owner3"],
-            "dir_with_slash/testfile.txt": ["@owner2"],
-            "dir_without_slash/testfile.txt": ["@owner4"],
-            "dir_with_slash/subdir_with_slash/": ["@owner5"],
-            "dir_with_slash/subdir_with_slash/testfile.txt": ["@owner6"],
-            "dir_with_slash/subdir_without_slash/": ["@owner7"],
-            "dir_with_slash/subdir_without_slash/testfile.txt": ["@owner8"],
-        },
-        stderr=helpers.dedent(
-            """
-            Synchronizing dependencies
-            """
-        ),
-    )
-
-
-def test_ownership_location(
-    dda: CliRunner,
-    temp_dir: Path,
-    create_codeowners_file: Callable[[dict[str, list[str]], Path], None],
-    create_temp_items: Callable[[Iterable[str], Path], None],
-    helpers: ModuleType,
-) -> None:
-    ownership_data = {
-        "testfile.txt": ["@owner1"],
-    }
-    expected_result = ownership_data
-    custom_location = Path("custom/CODEOWNERS")
-    create_codeowners_file(ownership_data, temp_dir / custom_location)
-    create_temp_items(ownership_data.keys(), temp_dir)
-    with temp_dir.as_cwd():
-        result = dda(
-            "info",
-            "owners",
-            "code",
-            "--json",
-            "--owners",
-            custom_location.as_posix(),
-            *expected_result.keys(),
+            *paths_to_test,
         )
 
     result.check(
@@ -218,44 +126,65 @@ def test_ownership_location(
     )
 
 
+def test_ownership_location(
+    dda: CliRunner,
+    helpers: ModuleType,
+) -> None:
+    testcase_result = TESTCASE_RESULTS[4]
+    with (Path(__file__).parent / "fixtures" / "test4").as_cwd():
+        result = dda(
+            "info",
+            "owners",
+            "code",
+            "--json",
+            "--owners",
+            "../custom_CODEOWNERS",
+            *testcase_result.keys(),
+        )
+
+    result.check(
+        exit_code=0,
+        stdout_json=testcase_result,
+        stderr=helpers.dedent(
+            """
+            Synchronizing dependencies
+            """
+        ),
+    )
+
+
 def test_human_output(
     dda: CliRunner,
-    temp_dir: Path,
     helpers: ModuleType,
-    create_codeowners_file: Callable[[dict[str, list[str]], Path], None],
-    create_temp_items: Callable[[Iterable[str], Path], None],
-    default_codeowners_location: Path,
 ) -> None:
-    ownership_data = {
-        "testfile.txt": ["@owner1"],
-        "subdir/testfile2.txt": ["@owner2"],
-        "subdir/anotherfile.txt": ["@owner1", "@owner3"],
-    }
+    testcase_result = TESTCASE_RESULTS[4]
 
-    create_codeowners_file(ownership_data, temp_dir / default_codeowners_location)
-    create_temp_items(ownership_data.keys(), temp_dir)
-    with temp_dir.as_cwd():
-        result = dda("info", "owners", "code", *ownership_data.keys())
+    with (Path(__file__).parent / "fixtures" / "test4").as_cwd():
+        result = dda("info", "owners", "code", *testcase_result.keys())
 
     result.check(
         exit_code=0,
         stdout=helpers.dedent(
             """
-            ┌────────────────────────┬──────────────────┐
-            │ testfile.txt           │ @owner1          │
-            │ subdir/testfile2.txt   │ @owner2          │
-            │ subdir/anotherfile.txt │ @owner1, @owner3 │
-            └────────────────────────┴──────────────────┘
+            ┌──────────────────────┬─────────────────────────────────────────┐
+            │ test.txt             │ @DataDog/team-everything                │
+            │ README.md            │ @DataDog/team-devops, @DataDog/team-doc │
+            │ .gitlab/             │ @DataDog/team-devops                    │
+            │ .gitlab/security.yml │ @DataDog/team-security                  │
+            │ .gitlab/ci.yml       │ @DataDog/team-devops                    │
+            └──────────────────────┴─────────────────────────────────────────┘
             """
         ),
         output=helpers.dedent(
             """
             Synchronizing dependencies
-            ┌────────────────────────┬──────────────────┐
-            │ testfile.txt           │ @owner1          │
-            │ subdir/testfile2.txt   │ @owner2          │
-            │ subdir/anotherfile.txt │ @owner1, @owner3 │
-            └────────────────────────┴──────────────────┘
+            ┌──────────────────────┬─────────────────────────────────────────┐
+            │ test.txt             │ @DataDog/team-everything                │
+            │ README.md            │ @DataDog/team-devops, @DataDog/team-doc │
+            │ .gitlab/             │ @DataDog/team-devops                    │
+            │ .gitlab/security.yml │ @DataDog/team-security                  │
+            │ .gitlab/ci.yml       │ @DataDog/team-devops                    │
+            └──────────────────────┴─────────────────────────────────────────┘
             """
         ),
     )
--- tests/cli/info/owners/test_format.py
+++ tests/cli/info/owners/test_format.py
@@ -2,23 +2,18 @@ from dda.cli.info.owners.format import format_path_for_codeowners
 from dda.utils.fs import Path
 
 
-def test_format_path_for_codeowners(temp_dir, create_temp_file_or_dir):
-    with temp_dir.as_cwd():
+def test_format_path_for_codeowners():
+    with (Path(__file__).parent / "fixtures" / "test_format").as_cwd():
         # Test 1: Subpath is a file
-        create_temp_file_or_dir(temp_dir / "some" / "path", force_file=True)
-
         assert format_path_for_codeowners(Path("some/path")) == "some/path"
         assert format_path_for_codeowners(Path("some/path/")) == "some/path"
         assert format_path_for_codeowners(Path("./some/path")) == "some/path"
 
         # Test 2: Subpath is a directory
-        create_temp_file_or_dir(temp_dir / "some" / "path2", force_file=False)
         assert format_path_for_codeowners(Path("some/path2/")) == "some/path2/"
         assert format_path_for_codeowners(Path("some/path2")) == "some/path2/"
         assert format_path_for_codeowners(Path("./some/path2")) == "some/path2/"
 
         # Test 2: Subpath is a directory with an extension
-        create_temp_file_or_dir(temp_dir / "some" / "path.ext" / "file.txt", force_file=True)
-
         assert format_path_for_codeowners(Path("some/path.ext/")) == "some/path.ext/"
         assert format_path_for_codeowners(Path("some/path.ext/file.txt")) == "some/path.ext/file.txt"
