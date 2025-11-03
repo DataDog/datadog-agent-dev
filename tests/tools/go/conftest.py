fea5456398802a2a1bbb574030b27dc4bf6354a7
Pierre-Louis Veyrenc <pierrelouis.veyrenc@datadoghq.com>
2025-11-03 13:35:29 +0100

    fix(build): Prevent `get_random_filename` from creating filenames starting with `-`
    This would cause a failure in `test_command_formation` if the entrypoint starts with `-`, as it would then be considered a flag instead of a parameter

--- tests/tools/go/conftest.py
+++ tests/tools/go/conftest.py
@@ -12,8 +12,10 @@ AVAILABLE_CHARS = string.ascii_letters + string.digits + "-_+.!@#$%^&*()[]{}:;,.
 def get_random_filename():
     def _get_random_filename(k: int = 10, root: Path | None = None) -> Path:
         name = "".join(random.choices(AVAILABLE_CHARS, k=k))
+        # Remove the leading `-` to avoid considering it as a command flag
+        path = Path(name.removeprefix("-"))
         if root:
-            return root / name
-        return Path(name)
+            return root / path
+        return path
 
     return _get_random_filename
