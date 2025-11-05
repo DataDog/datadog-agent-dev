0d68e744b1897dd19244170da89559de1e9a1801
Pierre-Louis Veyrenc <pierrelouis.veyrenc@datadoghq.com>
2025-11-05 17:33:02 +0100

    refactor(build): Get artifact components from `BuildArtifact` class instance, instead of parsing from calling command

--- src/dda/build/artifacts/base.py
+++ src/dda/build/artifacts/base.py
@@ -6,9 +6,13 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
+from dda.build.metadata.metadata import BuildMetadata, analyze_context
+
 if TYPE_CHECKING:
     from typing import Any
 
+    from dda.build.metadata.digests import ArtifactDigest
+    from dda.build.metadata.formats import ArtifactFormat
     from dda.cli.application import Application
 
 
@@ -23,3 +27,20 @@ class BuildArtifact(ABC):
         """
         Build the artifact. This function can have arbitrary side effects (creating files, running commands, etc.), and is not expected to return anything.
         """
+
+    @abstractmethod
+    def get_build_components(self) -> tuple[set[str], ArtifactFormat]:
+        """
+        Gets the build components and artifact format for this artifact.
+
+        Returns:
+            A tuple containing the build components and artifact format for this artifact.
+        """
+
+    def compute_metadata(self, app: Application, artifact_digest: ArtifactDigest) -> BuildMetadata:
+        """
+        Creates a BuildMetadata instance for this artifact.
+        """
+        from dda.build.metadata.metadata import BuildMetadata
+
+        return BuildMetadata.spawn_from_context(analyze_context(app), self.get_build_components(), artifact_digest)
