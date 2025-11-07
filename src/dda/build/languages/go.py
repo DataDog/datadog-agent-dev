5890c482113de409b06139c4922bf6ecf8416324
Pierre-Louis Veyrenc <pierrelouis.veyrenc@datadoghq.com>
2025-11-07 12:58:42 +0100

    feat(build): Create `GoArtifact` class
    This proposed mechanism should allow us to define required functions for every possible artifact language. In this example, Go artifacts need to define build tags, build flags etc.

---
+++ src/dda/build/languages/go.py
@@ -0,0 +1,39 @@
+# SPDX-FileCopyrightText: 2025-present Datadog, Inc. <dev@datadoghq.com>
+#
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+
+
+class GoArtifact(ABC):
+    """
+    Base class for all Go artifacts.
+    Any artifact class for an artifact built with the Go compiler should inherit from this class and implement its methods.
+    """
+
+    @abstractmethod
+    def get_build_tags(self) -> set[str]:
+        """
+        Get the build tags to pass to the Go compiler for the artifact.
+        """
+
+    @abstractmethod
+    def get_gcflags(self) -> list[str]:
+        """
+        Get the gcflags to pass to the Go compiler for the artifact.
+        """
+
+    @abstractmethod
+    def get_ldflags(self) -> list[str]:
+        """
+        Get the ldflags to pass to the Go compiler for the artifact.
+        """
+        return []
+
+    @abstractmethod
+    def get_build_env(self) -> dict[str, str]:
+        """
+        Get the build environment variables to pass to the Go compiler for the artifact.
+        """
