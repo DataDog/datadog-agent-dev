2c0157c92d725200edb1532caefa838f9d7b3460
Pierre-Louis Veyrenc <pierrelouis.veyrenc@datadoghq.com>
2025-11-07 14:24:43 +0100

    feat(go): Make `GOPATH` and `GOCACHE` user-configurable and handled via `dda`

--- tests/cli/config/test_show.py
+++ tests/cli/config/test_show.py
@@ -6,8 +6,12 @@ from __future__ import annotations
 from dda.env.dev import DEFAULT_DEV_ENV
 
 
-def test_default_scrubbed(dda, config_file, helpers, default_cache_dir, default_data_dir, default_git_author):
+def test_default_scrubbed(
+    dda, config_file, helpers, default_cache_dir, default_data_dir, default_git_author, default_gopath, default_gocache
+):
     config_file.data["github"]["auth"] = {"user": "foo", "token": "bar"}
+    config_file.data["tools"]["go"]["gopath"] = str(default_gopath)
+    config_file.data["tools"]["go"]["gocache"] = str(default_gocache)
     config_file.save()
 
     result = dda("config", "show")
@@ -33,6 +37,10 @@ def test_default_scrubbed(dda, config_file, helpers, default_cache_dir, default_
             [tools.git.author]
             name = "{default_git_author.name}"
             email = "{default_git_author.email}"
+
+            [tools.go]
+            gopath = "{default_gopath}"
+            gocache = "{default_gocache}"
 
             [storage]
             data = "{default_data_directory}"
@@ -71,8 +79,12 @@ def test_default_scrubbed(dda, config_file, helpers, default_cache_dir, default_
     )
 
 
-def test_reveal(dda, config_file, helpers, default_cache_dir, default_data_dir, default_git_author):
+def test_reveal(
+    dda, config_file, helpers, default_cache_dir, default_data_dir, default_git_author, default_gopath, default_gocache
+):
     config_file.data["github"]["auth"] = {"user": "foo", "token": "bar"}
+    config_file.data["tools"]["go"]["gopath"] = str(default_gopath)
+    config_file.data["tools"]["go"]["gocache"] = str(default_gocache)
     config_file.save()
 
     result = dda("config", "show", "-a")
@@ -98,6 +110,10 @@ def test_reveal(dda, config_file, helpers, default_cache_dir, default_data_dir, 
             [tools.git.author]
             name = "{default_git_author.name}"
             email = "{default_git_author.email}"
+
+            [tools.go]
+            gopath = "{default_gopath}"
+            gocache = "{default_gocache}"
 
             [storage]
             data = "{default_data_directory}"
