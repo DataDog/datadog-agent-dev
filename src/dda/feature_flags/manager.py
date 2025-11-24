f8cb0d1013be27f1db007ced622824491af4c998
Celian Raimbault <celian.raimbault@datadoghq.com>
2025-11-24 15:18:23 +0100

    [feature-flags-more-data] Added user

--- src/dda/feature_flags/manager.py
+++ src/dda/feature_flags/manager.py
@@ -205,6 +205,7 @@ class CIFeatureFlagManager(FeatureFlagManager):
             "ci.job.id": os.getenv("CI_JOB_ID", ""),
             "ci.stage.name": os.getenv("CI_JOB_STAGE", ""),
             "git.branch": os.getenv("CI_COMMIT_BRANCH", ""),
+            "user": os.getenv("CI_COMMIT_AUTHOR", ""),
         }
 
 
